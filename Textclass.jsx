a237a5b0eb6b74e4cd48d34c9e47113adad23e2b
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-04-11 18:29:10 +0530

    Create Textclass.jsx

---
+++ Textclass.jsx
@@ -0,0 +1,206 @@
+export function Text() {
+    return (
+        <div>
+            <h1 class="text-6xl text-blue-800 font-bold text-center">Tailwind Text Colors cheat sheet</h1>
+            <div class="grid grid-cols-7 gap-4 text-sm ml-14">
+                <div>
+                    <h1 class="mb-2 text-red-500 font-bold text-4xl">RED</h1>
+                    <p class="text-red-50 font-bold">text-red-50</p>
+                    <p class="text-red-100 font-bold">text-red-100</p>
+                    <p class="text-red-200 font-bold">text-red-200</p>
+                    <p class="text-red-300 font-bold">text-red-300</p>
+                    <p class="text-red-400 font-bold">text-red-400</p>
+                    <p class="text-red-500 font-bold">text-red-500</p>
+                    <p class="text-red-600 font-bold">text-red-600</p>
+                    <p class="text-red-700 font-bold">text-red-700</p>
+                    <p class="text-red-800 font-bold">text-red-800</p>
+                    <p class="text-red-900 font-bold">text-red-900</p>
+                    <p class="text-red-950 font-bold">text-red-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-blue-500 font-bold text-4xl">BLUE</h1>
+                    <p class="text-blue-50 font-bold ">text-blue-50</p>
+                    <p class="text-blue-100 font-bold">text-blue-100</p>
+                    <p class="text-blue-200 font-bold">text-blue-200</p>
+                    <p class="text-blue-300 font-bold">text-blue-300</p>
+                    <p class="text-blue-400 font-bold">text-blue-400</p>
+                    <p class="text-blue-500 font-bold">text-blue-500</p>
+                    <p class="text-blue-600 font-bold">text-blue-600</p>
+                    <p class="text-blue-700 font-bold">text-blue-700</p>
+                    <p class="text-blue-800 font-bold">text-blue-800</p>
+                    <p class="text-blue-900 font-bold">text-blue-900</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-green-500 font-bold text-4xl">GREEN</h1>
+                    <p class="text-green-50 font-bold ">text-green-50</p>
+                    <p class="text-green-100 font-bold ">text-green-100</p>
+                    <p class="text-green-200 font-bold ">text-green-200</p>
+                    <p class="text-green-300 font-bold ">text-green-300</p>
+                    <p class="text-green-400 font-bold ">text-green-400</p>
+                    <p class="text-green-500 font-bold ">text-green-500</p>
+                    <p class="text-green-600 font-bold ">text-green-600</p>
+                    <p class="text-green-700 font-bold ">text-green-700</p>
+                    <p class="text-green-800 font-bold ">text-green-800</p>
+                    <p class="text-green-900 font-bold ">text-green-900</p>
+                    <p class="text-green-950 font-bold ">text-green-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-yellow-500 font-bold text-4xl">YELLOW</h1>
+                    <p class="text-yellow-50 font-bold ">text-yellow-50</p>
+                    <p class="text-yellow-100 font-bold ">text-yellow-100</p>
+                    <p class="text-yellow-200 font-bold ">text-yellow-200</p>
+                    <p class="text-yellow-300 font-bold ">text-yellow-300</p>
+                    <p class="text-yellow-400 font-bold ">text-yellow-400</p>
+                    <p class="text-yellow-500 font-bold ">text-yellow-500</p>
+                    <p class="text-yellow-600 font-bold ">text-yellow-600</p>
+                    <p class="text-yellow-700 font-bold ">text-yellow-700</p>
+                    <p class="text-yellow-800 font-bold ">text-yellow-800</p>
+                    <p class="text-yellow-900 font-bold ">text-yellow-900</p>
+                    <p class="text-yellow-950 font-bold ">text-yellow-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-purple-500 font-bold text-4xl">PURPLE</h1>
+                    <p class="text-purple-50 font-bold">text-purple-50</p>
+                    <p class="text-purple-100 font-bold">text-purple-100</p>
+                    <p class="text-purple-200 font-bold">text-purple-200</p>
+                    <p class="text-purple-300 font-bold">text-purple-300</p>
+                    <p class="text-purple-400 font-bold">text-purple-400</p>
+                    <p class="text-purple-500 font-bold">text-purple-500</p>
+                    <p class="text-purple-600 font-bold">text-purple-600</p>
+                    <p class="text-purple-700 font-bold">text-purple-700</p>
+                    <p class="text-purple-800 font-bold">text-purple-800</p>
+                    <p class="text-purple-900 font-bold">text-purple-900</p>
+                    <p class="text-purple-950 font-bold">text-purple-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-pink-500 font-bold text-4xl">PINK</h1>
+                    <p class="text-pink-50 font-bold">text-pink-50</p>
+                    <p class="text-pink-100 font-bold">text-pink-100</p>
+                    <p class="text-pink-200 font-bold">text-pink-200</p>
+                    <p class="text-pink-300 font-bold">text-pink-300</p>
+                    <p class="text-pink-400 font-bold">text-pink-400</p>
+                    <p class="text-pink-500 font-bold">text-pink-500</p>
+                    <p class="text-pink-600 font-bold">text-pink-600</p>
+                    <p class="text-pink-700 font-bold">text-pink-700</p>
+                    <p class="text-pink-800 font-bold">text-pink-800</p>
+                    <p class="text-pink-900 font-bold">text-pink-900</p>
+                    <p class="text-pink-950 font-bold">text-pink-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-gray-500 font-bold text-4xl">GRAY</h1>
+                    <p class="text-gray-50 font-bold">text-gray-50</p>
+                    <p class="text-gray-100 font-bold">text-gray-100</p>
+                    <p class="text-gray-200 font-bold">text-gray-200</p>
+                    <p class="text-gray-300 font-bold">text-gray-300</p>
+                    <p class="text-gray-400 font-bold">text-gray-400</p>
+                    <p class="text-gray-500 font-bold">text-gray-500</p>
+                    <p class="text-gray-600 font-bold">text-gray-600</p>
+                    <p class="text-gray-700 font-bold">text-gray-700</p>
+                    <p class="text-gray-800 font-bold">text-gray-800</p>
+                    <p class="text-gray-900 font-bold">text-gray-900</p>
+                    <p class="text-gray-950 font-bold">text-gray-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-indigo-500 font-bold text-4xl">INDIGO</h1>
+                    <p class="text-indigo-50 font-bold">text-indigo-50</p>
+                    <p class="text-indigo-100 font-bold">text-indigo-100</p>
+                    <p class="text-indigo-200 font-bold">text-indigo-200</p>
+                    <p class="text-indigo-300 font-bold">text-indigo-300</p>
+                    <p class="text-indigo-400 font-bold">text-indigo-400</p>
+                    <p class="text-indigo-500 font-bold">text-indigo-500</p>
+                    <p class="text-indigo-600 font-bold">text-indigo-600</p>
+                    <p class="text-indigo-700 font-bold">text-indigo-700</p>
+                    <p class="text-indigo-800 font-bold">text-indigo-800</p>
+                    <p class="text-indigo-900 font-bold">text-indigo-900</p>
+                    <p class="text-indigo-950 font-bold">text-indigo-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-cyan-500 font-bold text-4xl">CYAN</h1>
+                    <p class="text-cyan-50 font-bold">text-cyan-50</p>
+                    <p class="text-cyan-100 font-bold">text-cyan-100</p>
+                    <p class="text-cyan-200 font-bold">text-cyan-200</p>
+                    <p class="text-cyan-300 font-bold">text-cyan-300</p>
+                    <p class="text-cyan-400 font-bold">text-cyan-400</p>
+                    <p class="text-cyan-500 font-bold">text-cyan-500</p>
+                    <p class="text-cyan-600 font-bold">text-cyan-600</p>
+                    <p class="text-cyan-700 font-bold">text-cyan-700</p>
+                    <p class="text-cyan-800 font-bold">text-cyan-800</p>
+                    <p class="text-cyan-900 font-bold">text-cyan-900</p>
+                    <p class="text-cyan-950 font-bold">text-cyan-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-teal-500 font-bold text-4xl">TEAL</h1>
+                    <p class="text-teal-50 font-bold">text-teal-50</p>
+                    <p class="text-teal-100 font-bold">text-teal-100</p>
+                    <p class="text-teal-200 font-bold">text-teal-200</p>
+                    <p class="text-teal-300 font-bold">text-teal-300</p>
+                    <p class="text-teal-400 font-bold">text-teal-400</p>
+                    <p class="text-teal-500 font-bold">text-teal-500</p>
+                    <p class="text-teal-600 font-bold">text-teal-600</p>
+                    <p class="text-teal-700 font-bold">text-teal-700</p>
+                    <p class="text-teal-800 font-bold">text-teal-800</p>
+                    <p class="text-teal-900 font-bold">text-teal-900</p>
+                    <p class="text-teal-950 font-bold">text-teal-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-amber-500 font-bold text-4xl">AMBER</h1>
+                    <p class="text-amber-50 font-bold">text-amber-50</p>
+                    <p class="text-amber-100 font-bold">text-amber-100</p>
+                    <p class="text-amber-200 font-bold">text-amber-200</p>
+                    <p class="text-amber-300 font-bold">text-amber-300</p>
+                    <p class="text-amber-400 font-bold">text-amber-400</p>
+                    <p class="text-amber-500 font-bold">text-amber-500</p>
+                    <p class="text-amber-600 font-bold">text-amber-600</p>
+                    <p class="text-amber-700 font-bold">text-amber-700</p>
+                    <p class="text-amber-800 font-bold">text-amber-800</p>
+                    <p class="text-amber-900 font-bold">text-amber-900</p>
+                    <p class="text-amber-950 font-bold">text-amber-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-orange-500 font-bold text-4xl">ORANGE</h1>
+                    <p class="text-orange-50 font-bold">text-orange-50</p>
+                    <p class="text-orange-100 font-bold">text-orange-100</p>
+                    <p class="text-orange-200 font-bold">text-orange-200</p>
+                    <p class="text-orange-300 font-bold">text-orange-300</p>
+                    <p class="text-orange-400 font-bold">text-orange-400</p>
+                    <p class="text-orange-500 font-bold">text-orange-500</p>
+                    <p class="text-orange-600 font-bold">text-orange-600</p>
+                    <p class="text-orange-700 font-bold">text-orange-700</p>
+                    <p class="text-orange-800 font-bold">text-orange-800</p>
+                    <p class="text-orange-900 font-bold">text-orange-900</p>
+                    <p class="text-orange-950 font-bold">text-orange-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-rose-500 font-bold text-4xl">ROSE</h1>
+                    <p class="text-rose-50 font-bold">text-rose-50</p>
+                    <p class="text-rose-100 font-bold">text-rose-100</p>
+                    <p class="text-rose-200 font-bold">text-rose-200</p>
+                    <p class="text-rose-300 font-bold">text-rose-300</p>
+                    <p class="text-rose-400 font-bold">text-rose-400</p>
+                    <p class="text-rose-500 font-bold">text-rose-500</p>
+                    <p class="text-rose-600 font-bold">text-rose-600</p>
+                    <p class="text-rose-700 font-bold">text-rose-700</p>
+                    <p class="text-rose-800 font-bold">text-rose-800</p>
+                    <p class="text-rose-900 font-bold">text-rose-900</p>
+                    <p class="text-rose-950 font-bold">text-rose-950</p>
+                </div>
+                <div>
+                    <h1 class="mb-2 text-zinc-500 font-bold text-4xl">ZINC</h1>
+                    <p class="text-zinc-50 font-bold">text-zinc-50</p>
+                    <p class="text-zinc-100 font-bold">text-zinc-100</p>
+                    <p class="text-zinc-200 font-bold">text-zinc-200</p>
+                    <p class="text-zinc-300 font-bold">text-zinc-300</p>
+                    <p class="text-zinc-400 font-bold">text-zinc-400</p>
+                    <p class="text-zinc-500 font-bold">text-zinc-500</p>
+                    <p class="text-zinc-600 font-bold">text-zinc-600</p>
+                    <p class="text-zinc-700 font-bold">text-zinc-700</p>
+                    <p class="text-zinc-800 font-bold">text-zinc-800</p>
+                    <p class="text-zinc-900 font-bold">text-zinc-900</p>
+                    <p class="text-zinc-950 font-bold">text-zinc-950</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+// https://www.notion.so/Tailwind-Text-Colors-Cheat-Sheet-33f14675ed4d8002b097c3f6f23ee292?source=copy_link
