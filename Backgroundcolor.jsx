3c291b864e992ba10c838c12c9ce911403ffb1eb
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2026-04-18 18:21:45 +0530

    Create Backgroundcolor.jsx

---
+++ Backgroundcolor.jsx
@@ -0,0 +1,179 @@
+export function Allbackgrounds() {
+    return (
+        <div>
+            <h1 class="text-6xl text-blue-800 font-bold text-center">Tailwind Background Colors cheat sheet</h1>
+            <div class="grid grid-cols-12 gap-4 text-sm ml-14">
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-red-500 font-bold text-4xl">RED</h1>
+                    <div class="bg-red-50 font-bold  w-20 h-10 text-black flex items-center justify-center shadow-lg">bg-red-50</div>
+                    <div class="bg-red-100 font-bold w-20 h-10 text-black flex items-center justify-center shadow-lg">bg-red-100</div>
+                    <div class="bg-red-200 font-bold w-20 h-10 text-black flex items-center justify-center shadow-lg">bg-red-200</div>
+                    <div class="bg-red-300 font-bold w-20 h-10 text-black flex items-center justify-center shadow-lg">bg-red-300</div>
+                    <div class="bg-red-400 font-bold w-20 h-10 text-white flex items-center justify-center shadow-lg">bg-red-400</div>
+                    <div class="bg-red-500 font-bold w-20 h-10 text-white flex items-center justify-center shadow-lg">bg-red-500</div>
+                    <div class="bg-red-600 font-bold w-20 h-10 text-white flex items-center justify-center shadow-lg">bg-red-600</div>
+                    <div class="bg-red-700 font-bold w-20 h-10 text-white flex items-center justify-center shadow-lg">bg-red-700</div>
+                    <div class="bg-red-800 font-bold w-20 h-10 text-white flex items-center justify-center shadow-lg">bg-red-800</div>
+                    <div class="bg-red-900 font-bold w-20 h-10 text-white flex items-center justify-center shadow-lg">bg-red-900</div>
+                    <div class="bg-red-950 font-bold w-20 h-10 text-white flex items-center justify-center shadow-lg">bg-red-950</div>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-blue-500 font-bold text-4xl">BLUE</h1>
+                    <div class="bg-blue-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-blue-50</div>
+                    <div class="bg-blue-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-blue-100</div>
+                    <div class="bg-blue-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-blue-200</div>
+                    <div class="bg-blue-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-blue-300</div>
+                    <div class="bg-blue-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-blue-400</div>
+                    <div class="bg-blue-500 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-blue-500</div>
+                    <div class="bg-blue-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-blue-600</div>
+                    <div class="bg-blue-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-blue-700</div>
+                    <div class="bg-blue-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-blue-800</div>
+                    <div class="bg-blue-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-blue-900</div>
+                    <div class="bg-blue-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-blue-950</div>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-green-500 font-bold text-4xl">GREEN</h1>
+                    <div class="bg-green-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-green-50</div>
+                    <div class="bg-green-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-green-100</div>
+                    <div class="bg-green-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-green-200</div>
+                    <div class="bg-green-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-green-300</div>
+                    <div class="bg-green-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-green-400</div>
+                    <div class="bg-green-500 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-green-500</div>
+                    <div class="bg-green-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-green-600</div>
+                    <div class="bg-green-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-green-700</div>
+                    <div class="bg-green-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-green-800</div>
+                    <div class="bg-green-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-green-900</div>
+                    <div class="bg-green-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-green-950</div>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-yellow-500 font-bold text-4xl">YELLOW</h1>
+                    <div class="bg-yellow-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-yellow-50</div>
+                    <div class="bg-yellow-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-yellow-100</div>
+                    <div class="bg-yellow-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-yellow-200</div>
+                    <div class="bg-yellow-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-yellow-300</div>
+                    <div class="bg-yellow-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-yellow-400</div>
+                    <div class="bg-yellow-500 w-20 h-10 flex items-center justify-center shadow-lg">bg-yellow-500</div>
+                    <div class="bg-yellow-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-yellow-600</div>
+                    <div class="bg-yellow-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-yellow-700</div>
+                    <div class="bg-yellow-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-yellow-800</div>
+                    <div class="bg-yellow-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-yellow-900</div>
+                    <div class="bg-yellow-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-yellow-950</div>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-purple-500 font-bold text-4xl">PURPLE</h1>
+                    <div class="bg-purple-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-purple-50</div>
+                    <div class="bg-purple-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-purple-100</div>
+                    <div class="bg-purple-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-purple-200</div>
+                    <div class="bg-purple-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-purple-300</div>
+                    <div class="bg-purple-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-purple-400</div>
+                    <div class="bg-purple-500 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-purple-500</div>
+                    <div class="bg-purple-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-purple-600</div>
+                    <div class="bg-purple-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-purple-700</div>
+                    <div class="bg-purple-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-purple-800</div>
+                    <div class="bg-purple-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-purple-900</div>
+                    <div class="bg-purple-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-purple-950</div>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-pink-500 font-bold text-4xl">PINK</h1>
+                    <div class="bg-pink-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-pink-50</div>
+                    <div class="bg-pink-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-pink-100</div>
+                    <div class="bg-pink-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-pink-200</div>
+                    <div class="bg-pink-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-pink-300</div>
+                    <div class="bg-pink-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-pink-400</div>
+                    <div class="bg-pink-500 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-pink-500</div>
+                    <div class="bg-pink-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-pink-600</div>
+                    <div class="bg-pink-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-pink-700</div>
+                    <div class="bg-pink-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-pink-800</div>
+                    <div class="bg-pink-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-pink-900</div>
+                    <div class="bg-pink-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-pink-950</div>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-gray-500 font-bold text-4xl">GRAY</h1>
+                    <div class="bg-gray-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-gray-50</div>
+                    <div class="bg-gray-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-gray-100</div>
+                    <div class="bg-gray-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-gray-200</div>
+                    <div class="bg-gray-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-gray-300</div>
+                    <div class="bg-gray-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-gray-400</div>
+                    <div class="bg-gray-500 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-gray-500</div>
+                    <div class="bg-gray-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-gray-600</div>
+                    <div class="bg-gray-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-gray-700</div>
+                    <div class="bg-gray-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-gray-800</div>
+                    <div class="bg-gray-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-gray-900</div>
+                    <div class="bg-gray-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-gray-950</div>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-indigo-500 font-bold text-4xl">INDIGO</h1>
+                    <div class="bg-indigo-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-indigo-50</div>
+                    <div class="bg-indigo-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-indigo-100</div>
+                    <div class="bg-indigo-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-indigo-200</div>
+                    <div class="bg-indigo-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-indigo-300</div>
+                    <div class="bg-indigo-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-indigo-400</div>
+                    <div class="bg-indigo-500 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-indigo-500</div>
+                    <div class="bg-indigo-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-indigo-600</div>
+                    <div class="bg-indigo-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-indigo-700</div>
+                    <div class="bg-indigo-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-indigo-800</div>
+                    <div class="bg-indigo-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-indigo-900</div>
+                    <div class="bg-indigo-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-indigo-950</div>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-cyan-500 font-bold text-4xl">CYAN</h1>
+                    <div class="bg-cyan-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-cyan-50</div>
+                    <div class="bg-cyan-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-cyan-100</div>
+                    <div class="bg-cyan-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-cyan-200</div>
+                    <div class="bg-cyan-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-cyan-300</div>
+                    <div class="bg-cyan-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-cyan-400</div>
+                    <div class="bg-cyan-500 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-cyan-500</div>
+                    <div class="bg-cyan-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-cyan-600</div>
+                    <div class="bg-cyan-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-cyan-700</div>
+                    <div class="bg-cyan-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-cyan-800</div>
+                    <div class="bg-cyan-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-cyan-900</div>
+                    <div class="bg-cyan-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-cyan-950</div>
+                </div>
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-orange-500 font-bold text-4xl">ORANGE</h1>
+                    <div class="bg-orange-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-orange-50</div>
+                    <div class="bg-orange-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-orange-100</div>
+                    <div class="bg-orange-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-orange-200</div>
+                    <div class="bg-orange-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-orange-300</div>
+                    <div class="bg-orange-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-orange-400</div>
+                    <div class="bg-orange-500 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-orange-500</div>
+                    <div class="bg-orange-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-orange-600</div>
+                    <div class="bg-orange-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-orange-700</div>
+                    <div class="bg-orange-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-orange-800</div>
+                    <div class="bg-orange-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-orange-900</div>
+                    <div class="bg-orange-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-orange-950</div>
+                </div>
+                 <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-rose-500 font-bold text-4xl">ROSE</h1>
+                    <div class="bg-rose-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-rose-50</div>
+                    <div class="bg-rose-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-rose-100</div>
+                    <div class="bg-rose-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-rose-200</div>
+                    <div class="bg-rose-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-rose-300</div>
+                    <div class="bg-rose-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-rose-400</div>
+                    <div class="bg-rose-500 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-rose-500</div>
+                    <div class="bg-rose-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-rose-600</div>
+                    <div class="bg-rose-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-rose-700</div>
+                    <div class="bg-rose-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-rose-800</div>
+                    <div class="bg-rose-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-rose-900</div>
+                    <div class="bg-rose-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-rose-950</div>
+                </div>
+                 <div class="flex flex-col items-center justify-center gap-4">
+                    <h1 class="mb-2 text-teal-500 font-bold text-4xl">TEAL</h1>
+                    <div class="bg-teal-50 w-20 h-10 flex items-center justify-center shadow-lg">bg-teal-50</div>
+                    <div class="bg-teal-100 w-20 h-10 flex items-center justify-center shadow-lg">bg-teal-100</div>
+                    <div class="bg-teal-200 w-20 h-10 flex items-center justify-center shadow-lg">bg-teal-200</div>
+                    <div class="bg-teal-300 w-20 h-10 flex items-center justify-center shadow-lg">bg-teal-300</div>
+                    <div class="bg-teal-400 w-20 h-10 flex items-center justify-center shadow-lg">bg-teal-400</div>
+                    <div class="bg-teal-500 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-teal-500</div>
+                    <div class="bg-teal-600 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-teal-600</div>
+                    <div class="bg-teal-700 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-teal-700</div>
+                    <div class="bg-teal-800 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-teal-800</div>
+                    <div class="bg-teal-900 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-teal-900</div>
+                    <div class="bg-teal-950 w-20 h-10 flex items-center justify-center text-white shadow-lg">bg-teal-950</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+// https://www.notion.so/Tailwind-Text-Colors-Cheat-Sheet-33f14675ed4d8002b097c3f6f23ee292?source=copy_link
